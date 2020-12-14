ffba2b61c36a954058e7113eac31ddc12e740c93
mengang-sys <377532225@qq.com>
2020-12-14 14:29:37 +0800

    啊啊

--- src/main/java/com/my/batch/BatchApplication.java
+++ src/main/java/com/my/batch/BatchApplication.java
@@ -1,11 +1,7 @@
 package com.my.batch;
-
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.explore.support.SimpleJobExplorer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 @EnableBatchProcessing
@@ -15,12 +11,6 @@ public class BatchApplication {
 
         SpringApplication.run(BatchApplication.class, args);
 
-        /*ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("job.xml");
-        SimpleJobExplorer launcher = (SimpleJobExplorer) ctx.getBean("launcher");
-        Job job = (Job) ctx.getBean("jobExample");
-        System.out.println("=========="+launcher);
-        System.out.println("=========="+job);
-        launcher*/
     }
 
 }
